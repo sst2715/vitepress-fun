d1bbe4be3d01080c08c9221f6d98f8ff65e94351
sst2715 <leilei_2715@qq.com>
2024-11-12 14:18:35 +0800

    sst2715个性化

---
+++ .vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress-Fun',
+  themeConfig: {
+    logo: '/police.png',
+    siteTitle: 'VitePress-Fun',
+
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/sst2715' }
+    ]
+  }
+})
